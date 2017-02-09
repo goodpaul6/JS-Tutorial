b6edcca4c850029b69d25c93af726b8643066ed3
Apaar Madan <apaar_madan@hotmail.com>
2017-02-08 23:57:24 -0500

    first.

---
+++ main.js
@@ -0,0 +1,13 @@
+"use string";
+
+function calculate(str) {
+    // TODO: Actually write this function
+    return 0;
+}
+
+$(document).ready(function() {
+    $("#submit").click(function(e) {
+        var str = $("#str").val();
+        $("#result").text("Result: " + calculate(str).toString());        
+    });
+});
